4eedd65747db461660a2da5a657fc52ffee2b80c
Joshua Henry <henry41043@gmail.com>
2013-11-24 22:52:06 -0500

    jh - merged Jirakit's scenes and some updates to the collectitems script

---
+++ MainProject/Assets/Scenes/ScenesScripts/InstuctionPage4.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+//var customSkin: GUISkin;
+var NextStyle: GUIStyle;
+var BackStyle: GUIStyle;
+
+
+
+function OnGUI () {
+	var buttonWidth: int = 220;
+	var buttonHeight: int = 50;
+
+	//GUI.skin = customSkin;
+	
+	//Next Instuction 1 page
+	if(GUI.Button(Rect(700, 450,buttonWidth,buttonHeight),"Next",NextStyle))
+	{
+		Application.LoadLevel("Instuction5");
+	
+	}
+	//Back to title sence
+	if(GUI.Button(Rect(50, 450,buttonWidth,buttonHeight),"Back",BackStyle))
+	{
+		Application.LoadLevel("Instuction3");
+	
+	}
+	
+}
---
+++ MainProject/Assets/Scenes/ScenesScripts/TitleScript.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+var customSkin: GUISkin;
+//var startButton: GUISkin;
+var startButtonStyle: GUIStyle;
+var InstuctionButtonStyle: GUIStyle;
+
+
+
+function OnGUI () {
+	var buttonWidth: int = 220;
+	var buttonHeight: int = 50;
+	
+	//var halfScreenWidth: float = Screen.width/2;
+	//var halfButtonWidth: float = buttonWidth/2;
+	
+	GUI.skin = customSkin;
+	//GUI.skin = startButton;
+
+	//350,450
+	//start button
+	if(GUI.Button(Rect(350, 380,buttonWidth,buttonHeight),"WorldMap",startButtonStyle))
+	{
+		Application.LoadLevel("WorldMap");
+	
+	}
+	//Instuction button
+	if(GUI.Button(Rect(350, 450,buttonWidth,buttonHeight),"Instuction1",InstuctionButtonStyle))
+	{
+		Application.LoadLevel("Instuction1");
+	
+	}
+	
+}
