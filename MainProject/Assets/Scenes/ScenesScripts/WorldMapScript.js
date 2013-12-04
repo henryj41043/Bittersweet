c8aa48b204f90867834ae96fb3dd7af812211732
Henry <jdh84@n1-142-11.dhcp.drexel.edu>
2013-12-04 01:33:46 -0500

    jh - added new world map

--- MainProject/Assets/Scenes/ScenesScripts/WorldMapScript.js
+++ MainProject/Assets/Scenes/ScenesScripts/WorldMapScript.js
@@ -1,9 +1,9 @@
 ﻿#pragma strict
 var lollipopNode1: GUIStyle;
-private var lollipopNode1Width : int = 50;
-private var lollipopNode1Height : int = 50;
-private var lollipopNode1X : int = 149;
-private var lollipopNode1Y : int = 236;
+private var lollipopNode1Width : int = 40;
+private var lollipopNode1Height : int = 40;
+private var lollipopNode1X : int = 126;
+private var lollipopNode1Y : int = 244;
 
 function OnGUI () {	
 	if(GUI.Button(Rect(lollipopNode1X, lollipopNode1Y, lollipopNode1Width, lollipopNode1Height),"",lollipopNode1))
