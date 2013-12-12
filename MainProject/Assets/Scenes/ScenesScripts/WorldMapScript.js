c324ef4815e28a95dab5041c77a663823335f085
Henry <jdh84@71-202-09.local>
2013-12-11 22:04:11 -0500

    jh - more updates on worldmap

--- MainProject/Assets/Scenes/ScenesScripts/WorldMapScript.js
+++ MainProject/Assets/Scenes/ScenesScripts/WorldMapScript.js
@@ -12,20 +12,20 @@ private var starHeight : int = 100;
 private var backWidth : int = 140;
 private var backHeight : int = 80;
 
-private var castleX = 400;
-private var castleY = 295;
+private var castleX = 404;
+private var castleY = 306;
 
 private var backX = 20;
 private var backY = 515;
 
-private var lollipopLevel3X = 160;
-private var lollipopLevel3Y = 144;
+private var lollipopLevel3X = 142;
+private var lollipopLevel3Y = 146;
 
-private var lollipopLevel4X = 82;
-private var lollipopLevel4Y = 130;
+private var lollipopLevel4X = 80;
+private var lollipopLevel4Y = 142;
 
-private var lollipopLevel5X = 14;
-private var lollipopLevel5Y = 174;
+private var lollipopLevel5X = 22;
+private var lollipopLevel5Y = 188;
 
 function Start(){
 	if(!PlayerPrefs.HasKey("lollipopLevel3")){ PlayerPrefs.SetInt("lollipopLevel3", 0); }
@@ -34,26 +34,26 @@ function Start(){
 }
 
 function OnGUI () {
-	if(PlayerPrefs.GetInt("lollipopLevel3") == 1){
+	//if(PlayerPrefs.GetInt("lollipopLevel3") == 1){
 		if(GUI.Button(Rect(lollipopLevel3X, lollipopLevel3Y, buttonWidth, buttonHeight), "", lollipopLevel3Style))
 		{
 			Application.LoadLevel("Main");
 		}
-	}
+	//}
 	
-	if(PlayerPrefs.GetInt("lollipopLevel4") == 1){
+	//if(PlayerPrefs.GetInt("lollipopLevel4") == 1){
 		if(GUI.Button(Rect(lollipopLevel4X, lollipopLevel4Y, buttonWidth, buttonHeight), "", lollipopLevel4Style))
 		{
 		
 		}
-	}
+	//}
 	
-	if(PlayerPrefs.GetInt("lollipopLevel5") == 1){
+	//if(PlayerPrefs.GetInt("lollipopLevel5") == 1){
 		if(GUI.Button(Rect(lollipopLevel5X, lollipopLevel5Y, starWidth, starHeight), "", lollipopLevel5Style))
 		{
 		
 		}
-	}
+	//}
 	
 	if(GUI.Button(Rect(castleX, castleY, starWidth, starHeight), "", castleStyle))
 	{
